1fc7474642924c6351e64cf47827e4b88be6183f
ori livni <ori@orisomething.com>
2016-01-16 14:29:01 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Lodash modules snippets (for Atom)
+
+Import easily only the modules you need from [lodash](https://lodash.com/).
+Supports [lodash 4.0.0v](https://lodash.com/).
+
+## Examples
+- CommonJS: typing `r_map` will resolve to `var map = require('lodash/map');`
+- ES2015: typing `i_map` will resolve to `import map from 'lodash/map';`
+
+## Installation
+```
+apm install lodash-modules-snippets
+```
---
+++ create-snippets.js
@@ -0,0 +1,55 @@
+"use strict";
+
+const fs = require("fs");
+const path = require("path");
+const _ = require("lodash");
+
+
+const NATIVE_MODULES = Object.freeze([
+  "isFinite",
+  "isNaN",
+  "valueOf"
+]);
+
+const templateES = _.template(`
+  'ES <%= path %>':
+    'prefix': 'i_<%= name %>'
+    'body': """
+    import <%= imported %> from '<%= path %>';
+    """`
+);
+
+const templateCJS = _.template(`
+  'CJS <%= path %>':
+    'prefix': 'r_<%= name %>'
+    'body': """
+    const <%= imported %> = require('<%= path %>');
+    """`
+);
+
+const getImportedName = (name) => _.includes(NATIVE_MODULES, name) ? `_${name}` : name;
+
+const createSnippet = (templateFn, name) => templateFn({ name, imported: getImportedName(name), path: `lodash/${name}` });
+
+const createLodashSnippet = (templateFn) => templateFn({ name: 'lodash', imported: '_', path: 'lodash' });
+
+const createSnippets = (templateFn, modules) => _(modules)
+  .map(name => createSnippet(templateFn, name))
+  .concat([createLodashSnippet(templateFn)])
+  .join("")
+  .replace(/^/, "'.source.js':") +
+  "\n";
+
+const writeToFile = (fileName, str) => fs.writeFileSync(`keymaps/lodash-modules-snippets-${fileName}.cson`, str, "utf8");
+
+// Read the modules from library
+const modules = _.chain(fs.readdirSync(path.dirname(require.resolve("lodash"))))
+  .filter(fileName => /\.js$/.test(fileName))
+  .filter(fileName => fileName !== "index.js")
+  .filter(fileName => fileName !== "lodash.js")
+  .map(fileName => fileName.slice(0, fileName.length - 3))
+  .value();
+
+// Create snippets
+writeToFile("es", createSnippets(templateES, modules));
+writeToFile("cjs", createSnippets(templateCJS, modules));
